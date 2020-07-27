bb4db665703034616b1202cd2366ecb4efb88f6b
Tolga Can Unal <tolgacnunal@gmail.com>
2020-07-27 19:33:29 +0300

    Null Safe Refactor

--- app/src/main/java/com/tesleax/texster/CustomTypefaceSpan.kt
+++ app/src/main/java/com/tesleax/texster/CustomTypefaceSpan.kt
@@ -10,8 +10,8 @@ class CustomTypefaceSpan(private val font: Typeface?) : MetricAffectingSpan() {
     override fun updateDrawState(textPaint: TextPaint?) = update(textPaint)
 
     private fun update(tp: TextPaint?) {
-        tp.apply {
-            val old = this!!.typeface
+        tp?.apply {
+            val old = this.typeface
             val oldStyle = old?.style ?: 0
             val font = Typeface.create(font, oldStyle)
             typeface = font
